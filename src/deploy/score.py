377d3d4c6f7fabc75475a5b954eaaeca19be11c5
Keita Onabuta <keonabut@microsoft.com>
2022-11-02 15:57:11 +0000

    reformatted by black

--- src/deploy/score.py
+++ src/deploy/score.py
@@ -1,6 +1,8 @@
 ﻿import os
+
 import numpy as np
 from mlflow.pyfunc import load_model
+
 
 # Called when the service is loaded
 def init():
@@ -15,21 +17,14 @@ def init():
 
 
 def run(mini_batch):
-    print(
-        f"run method start: {__file__}, run({mini_batch})"
-    )
+    print(f"run method start: {__file__}, run({mini_batch})")
 
     for input in mini_batch:
-        input_np = np.loadtxt(
-            input, delimiter=",", skiprows=1
-        )
+        input_np = np.loadtxt(input, delimiter=",", skiprows=1)
         predictions = model.predict(input_np)
-        log_txt = "Predictions:" + str(
-            predictions
-        )
+        log_txt = "Predictions:" + str(predictions)
         print(log_txt)
 
     return [
-        [row, pred]
-        for row, pred in enumerate(predictions)
+        [row, pred] for row, pred in enumerate(predictions)
     ]  # return a dataframe or a list
--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -1,47 +1,30 @@
 # 本リポジトリで利用するデータを生成するスクリプト
-from azureml.core import (
-    Dataset,
-    Datastore,
-    Workspace,
-)
-from azureml.opendatasets import NycTlcGreen
-from datetime import datetime
-import pandas as pd
 import copy
 import os
+from datetime import datetime
+
+import pandas as pd
+from azureml.core import Dataset, Datastore, Workspace
+from azureml.opendatasets import NycTlcGreen
 from dateutil.relativedelta import relativedelta
 
 
 def register_dataset(ws: Workspace) -> None:
     dataset_name = "nyc_taxi_dataset"
     try:
-        dataset = Dataset.get_by_name(
-            ws, dataset_name
-        )
+        dataset = Dataset.get_by_name(ws, dataset_name)
         df = dataset.to_pandas_dataframe()
     except Exception:
         raw_df = pd.DataFrame([])
-        start = datetime.strptime(
-            "1/1/2015", "%m/%d/%Y"
-        )
-        end = datetime.strptime(
-            "1/31/2015", "%m/%d/%Y"
-        )
+        start = datetime.strptime("1/1/2015", "%m/%d/%Y")
+        end = datetime.strptime("1/31/2015", "%m/%d/%Y")
 
         for sample_month in range(3):
             temp_df_green = NycTlcGreen(
-                start
-                + relativedelta(
-                    months=sample_monthっs
-                ),
-                end
-                + relativedelta(
-                    months=sample_month
-                ),
+                start + relativedelta(months=sample_month),
+                end + relativedelta(months=sample_month),
             ).to_pandas_dataframe()
-            raw_df = raw_df.append(
-                temp_df_green.sample(2000)
-            )
+            raw_df = raw_df.append(temp_df_green.sample(2000))
 
         print(raw_df.head(10))
 
@@ -66,27 +49,15 @@ def register_dataset(ws: Workspace) -> None:
         for col in columns_to_remove:
             df.pop(col)
 
-        df = df.query(
-            "pickupLatitude>=40.53 and pickupLatitude<=40.88"
-        )
-        df = df.query(
-            "pickupLongitude>=-74.09 and pickupLongitude<=-73.72"
-        )
-        df = df.query(
-            "tripDistance>=0.25 and tripDistance<31"
-        )
-        df = df.query(
-            "passengerCount>0 and totalAmount>0"
-        )
+        df = df.query("pickupLatitude>=40.53 and pickupLatitude<=40.88")
+        df = df.query("pickupLongitude>=-74.09 and pickupLongitude<=-73.72")
+        df = df.query("tripDistance>=0.25 and tripDistance<31")
+        df = df.query("passengerCount>0 and totalAmount>0")
 
-        df["lpepPickupDatetime"] = df[
-            "lpepPickupDatetime"
-        ].map(lambda x: x.timestamp())
+        df["lpepPickupDatetime"] = df["lpepPickupDatetime"].map(lambda x: x.timestamp())
 
         datastore = Datastore.get_default(ws)
-        dataset = Dataset.Tabular.register_pandas_dataframe(
-            df, datastore, dataset_name
-        )
+        dataset = Dataset.Tabular.register_pandas_dataframe(df, datastore, dataset_name)
 
     print(df.head(5))
     df.to_csv(
