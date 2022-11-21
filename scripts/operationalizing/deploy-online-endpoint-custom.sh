3cf17e3869cfb4dede9dc6556b685ab42a533b4f
Keita Onabuta <keonabut@microsoft.com>
2022-11-21 02:33:49 +0000

    modify : online endpoint スクリプトの分割

--- scripts/operationalizing/deploy-online-endpoint-custom.sh
+++ scripts/operationalizing/deploy-online-endpoint-custom.sh
@@ -2,5 +2,3 @@
 az ml online-endpoint create -f ./jobs/online_endpoint.yml
 
 az ml online-deployment create -f ./jobs/online_deployment.yml
-
-az ml online-deployment create -f ./jobs/online_deployment_mlflow.yml
---
+++ scripts/operationalizing/deploy-online-endpoint-mlflow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+az ml online-endpoint create -f ./jobs/online_endpoint.yml
+
+az ml online-deployment create -f ./jobs/online_deployment_mlflow.yml
