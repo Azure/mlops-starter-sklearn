ec7a1cde274000696e954b610e1d4613e8e87b4a
Keita Onabuta <keonabut@microsoft.com>
2022-11-21 00:59:02 +0000

    modify : batch endpoint スクリプトの分割

--- scripts/operationalizing/deploy-batch-endpoint-mlflow.sh
+++ scripts/operationalizing/deploy-batch-endpoint-mlflow.sh
@@ -1,5 +1,3 @@
 az ml batch-endpoint create -f ./jobs/batch_endpoint.yml
 
-az ml batch-deployment create -f ./jobs/batch_deployment.yml
-
 az ml batch-deployment create -f ./jobs/batch_deployment_mlflow.yml
