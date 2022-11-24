8e64313b503811f0115da76223eb20d2e6c93203
Keita Onabuta <keonabut@microsoft.com>
2022-11-24 09:35:49 +0900

    Update scripts/operationalizing/deploy-online-endpoint-mlflow.sh
    Co-authored-by: Shohei Nagata <Shohei.Nagata@microsoft.com>

--- scripts/operationalizing/deploy-online-endpoint-mlflow.sh
+++ scripts/operationalizing/deploy-online-endpoint-mlflow.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-az ml online-endpoint create -f ./jobs/online_endpoint.yml
+export ENDPOINT_NAME=endpt-mlops-`echo $RANDOM`
+az ml online-endpoint create --name $ENDPOINT_NAME -f ./jobs/online_endpoint.yml
 
 az ml online-deployment create -f ./jobs/online_deployment_mlflow.yml
