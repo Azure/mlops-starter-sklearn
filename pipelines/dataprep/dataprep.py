5add0a41cfd6322ac0eaa089e05d8b881d9366f6
Keita Onabuta <keonabut@microsoft.com>
2022-11-24 09:03:33 +0000

    dataprep init

---
+++ pipelines/dataprep/dataprep.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+
+import matplotlib.pyplot as plt
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+
+def parse_args():
+    # 引数の処理
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_data", type=str, help="input data")
+    parser.add_argument("--training_data", type=str, help="Path of training_data")
+    parser.add_argument("--testing_data", type=str, help="Path of training_data")
+    args = parser.parse_args()
+    return args
+
+
+def process_data(df):
+    training_data, testing_data = train_test_split(
+        df, test_size=args.test_size, random_state=0
+    )
+    mlflow.log_metric("Train samples", len(training_data))
+    mlflow.log_metric("Test samples", len(testing_data))
+
+    # 分割データの出力
+    return training_data, testing_data
+
+
+def main(args):
+
+    # 引数の確認
+    lines = [
+        f"学習データのパス: {args.input_data}",
+        f"出力フォルダのパス: {args.output_dir}",
+    ]
+    [print(line) for line in lines]
+
+    # 学習データの読み込み
+    df = pd.read_csv(args.input_data)
+
+    # データ前処理
+    training_data, testing_data = process_data(df)
+    training_data.to_csv(Path(args.training_data) / "train.csv", index=False)
+    testing_data.to_csv(Path(args.testing_data) / "test.csv", index=False)
+
+
+if __name__ == "__main__":
+    # 引数の処理
+    args = parse_args()
+
+    # main 関数の実行
+    main(args)
