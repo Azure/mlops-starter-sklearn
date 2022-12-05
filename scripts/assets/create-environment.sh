56d2d52fb778ebb253fe493f20e88f9d28ca587c
Keita Onabuta <keonabut@microsoft.com>
2022-12-05 09:40:54 +0900

    Update scripts/assets/create-environment.sh
    既存 environment 有無の確認
    
    Co-authored-by: Kohei Ogawa <ruscello1023.kt@gmail.com>

--- scripts/assets/create-environment.sh
+++ scripts/assets/create-environment.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-az ml environment create -f ./assets/create-environment.yml
+# Get environment name from yaml file
+env_name=$(cat ./assets/create-environment.yml | grep name | awk '{print $2}')
+
+# Check if environment exists
+env_exists=$(az ml environment list --query "[?name=='$env_name']" | jq 'length')
+
+# If environment exists, do not create
+if [ $env_exists -gt 0 ]; then
+  echo "Environment already exists"
+else
+  # Create new environment 
+  az ml environment create -f ./assets/create-environment.yml
+fi
