f5db62969d9c6180045e1779cc78a38103fd421d
Keita Onabuta <keonabut@microsoft.com>
2022-12-05 09:37:25 +0900

    Update scripts/configure-workspace.sh
    おっしゃる通りです！
    
    Co-authored-by: Kohei Ogawa <ruscello1023.kt@gmail.com>

--- scripts/configure-workspace.sh
+++ scripts/configure-workspace.sh
@@ -3,6 +3,8 @@
 # 環境変数の読み込み
 source ./.env
 
+# 共同で利用しているサブスクリプションをセット
+az account set --subscription $SUBSCRIPTION
 # az ml デフォルトワークスペースの設定
 az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
 
