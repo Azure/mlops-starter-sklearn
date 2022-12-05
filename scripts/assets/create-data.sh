09a8ee5662e4f35ef12f74a6387b0cbb00e1bd1a
Keita Onabuta <keonabut@microsoft.com>
2022-12-05 09:38:44 +0900

    Update scripts/assets/create-data.sh
    既存 Data アセットの有無の確認
    
    Co-authored-by: Kohei Ogawa <ruscello1023.kt@gmail.com>

--- scripts/assets/create-data.sh
+++ scripts/assets/create-data.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-az ml data create -f ./assets/create-data.yml
+# Get dataset name from yaml file
+dataset_name=$(cat ./assets/create-data.yml | grep name | awk '{print $2}')
+
+# Check if dataset exists
+dataset_exists=$(az ml data list --query "[?name=='$dataset_name']" | jq 'length')
+
+# If dataset exists, do not create
+if [ $dataset_exists -gt 0 ]; then
+  echo "Dataset already exists"
+else
+  # Create new dataset
+  az ml data create -f ./assets/create-data.yml
+fi
