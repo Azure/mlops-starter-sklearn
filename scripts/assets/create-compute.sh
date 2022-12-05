7d2035d696c68ea9d5265374ec47d86e3f5b7ea1
Keita Onabuta <keonabut@microsoft.com>
2022-12-05 09:39:41 +0900

    Update scripts/assets/create-compute.sh
    既存 Compute 有無の確認
    
    Co-authored-by: Kohei Ogawa <ruscello1023.kt@gmail.com>

--- scripts/assets/create-compute.sh
+++ scripts/assets/create-compute.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-az ml compute create -f ./assets/create-compute.yml
+# Get cluster name from yaml file
+cluster_name=$(cat ./assets/create-compute.yml | grep name | awk '{print $2}')
+
+# Check if cluster exists
+cluster_exists=$(az ml compute list --query "[?name=='$cluster_name']" | jq 'length')
+
+# If cluster exists, do not create
+if [ $cluster_exists -gt 0 ]; then
+  echo "Cluster already exists"
+else
+  # Create new cluster
+  az ml compute create -f ./assets/create-compute.yml
+fi
