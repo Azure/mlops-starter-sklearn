3f60fb138cc43cb15831cb73e8c9b90ec0427dab
Keita Onabuta <keonabut@microsoft.com>
2022-12-15 10:51:45 +0900

    Update scripts/setup.sh
    Co-authored-by: Kohei Ogawa <ruscello1023.kt@gmail.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
 # conda 仮想環境
-conda env create -n mlops-train --file ./environments/conda_train.yml
-conda init
-conda activate mlops-train
+# conda環境名を指定
+CONDA_ENV_NAME="mlops-train"
+
+# 特定の環境が存在するかどうかを確認
+if conda env list | grep -q "$CONDA_ENV_NAME"; then
+    # check if the conda environment already exists
+    echo "Conda environment '$CONDA_ENV_NAME' already exists. Skipping creation."
+else
+    # 環境が存在しない場合はエラーメッセージを表示
+    conda env create -n $CONDA_ENV_NAME --file ../environments/conda_train.yml
+    
+    conda init bash
+    # check if the command was successful
+    if [ $? -eq 0 ]; then
+        echo "'conda init' command was successful."
+    fi
+
+    source ~/.bashrc
+    conda activate $CONDA_ENV_NAME
+    
+    # check if the command was successful
+    if [ $? -eq 0 ]; then
+        echo "'conda activate $CONDA_ENV_NAME' command was successful."
+    fi
+fi
 
 # pre-commit
 git init .
