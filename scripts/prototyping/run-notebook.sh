e5f0666b5983500b339b3c64830eb2ee82826207
Keita Onabuta <keonabut@microsoft.com>
2022-12-21 07:22:55 +0000

    add additional notebooks

--- scripts/prototyping/run-notebook.sh
+++ scripts/prototyping/run-notebook.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 conda activate mlops-train
 cd notebooks
-papermill train-prototyping.ipynb out.ipynb -k mlops-train
+papermill train-experiment.ipynb out.ipynb -k mlops-train
+papermill train-mlflow-local.ipynb out.ipynb -k mlops-train
+papermill train-model-debugging.ipynb out.ipynb -k mlops-train
