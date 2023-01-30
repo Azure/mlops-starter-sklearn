d543aa9b0965422a196ee896821bb4655abbec29
Keita Onabuta <keonabut@microsoft.com>
2023-01-30 06:46:45 +0000

    add space between lines

--- scripts/prototyping/run-notebooks.sh
+++ scripts/prototyping/run-notebooks.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 source /anaconda/etc/profile.d/conda.sh 
 conda activate mlops-train
+
 exec_path=$(readlink -f "$0")
 exec_dir=$(dirname "$exec_path")
 cd $exec_dir/../../notebooks
+
 papermill train-experiment.ipynb out.ipynb -k mlops-train
 papermill train-mlflow-local.ipynb out.ipynb -k mlops-train
 papermill train-model-debugging.ipynb out.ipynb -k mlops-train
