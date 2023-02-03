16bcdffc56d0341d685a75920b3d5114f7545ef3
keonabut@microsoft.com <keonabut@microsoft.com>
2023-02-03 02:44:12 +0000

    register model from the latest run

--- scripts/assets/register-model.sh
+++ scripts/assets/register-model.sh
@@ -5,5 +5,11 @@ exec_path=$(readlink -f "$0")
 exec_dir=$(dirname "$exec_path")
 cd $exec_dir/../../
 
-az ml model create -f ./cli/assets/register_model.yml
-az ml model create -f ./cli/assets/register_model_mlflow.yml
+# get the latest run id
+run_id=$(echo $(az ml job list --query "reverse(sort_by([?status=='Completed'].{experiment_name:experiment_name, run_id:name, status:status, date:creation_context.created_at}, &date))[0].run_id") | sed 's/"//g')
+echo $run_id
+
+
+# register model that was trained in the latest job
+az ml model create -f ./cli/assets/register_model.yml --path azureml://datastores/workspaceblobstore/paths/nyc-taxi/$run_id/models
+az ml model create -f ./cli/assets/register_model_mlflow.yml --path azureml://datastores/workspaceblobstore/paths/nyc-taxi/$run_id/models
